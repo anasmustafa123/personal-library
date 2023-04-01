e2a9ea916293595712ce38eef9202959286fd015
anasmuostafa <anasmuostafa@gmail.com>
2023-04-02 00:44:38 +0200

    toggling cards colors with  click on status button

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 let library = [];
-
+let statusOptions = ["to-read", "reading", "finished"];
 const cardsContainer = document.querySelector(".left-main");
 const title = document.getElementById("title");
 const author = document.getElementById("author");
@@ -13,29 +13,27 @@ function Book(title, author, pages, theStatus) {
   this.pages = pages;
   this.theStatus = theStatus;
 }
-
+function createNewElement(type, className, content) {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  element.textContent = content;
+  return element;
+}
 function addNewBook(e) {
   e.preventDefault();
-  library.push(new Book(title.value, author.value, pages.value, theStatus.value));
-  const newBook = document.createElement("div");
-  newBook.classList.add("main-grid-item");
+  library.push(
+    new Book(title.value, author.value, pages.value, theStatus.value)
+  );
+  const newBook = createNewElement("div", "main-grid-item", "");
   newBook.setAttribute("id", `${library.length}`);
-  const titleNode = document.createElement("div");
-  const authorNode = document.createElement("div");
-  const pagesNode = document.createElement("div");
-  const statusNode = document.createElement("div");
-  const removeButton = document.createElement("button");
-  titleNode.classList.add("title");
-  authorNode.classList.add("author");
-  pagesNode.classList.add("pages");
-  statusNode.classList.add("status");
-  removeButton.classList.add("remove-card", "clickable");
-  titleNode.textContent = title.value;
-  authorNode.textContent = author.value;
-  pagesNode.textContent = pages.value;
-  statusNode.textContent = theStatus.value;
-  removeButton.textContent = "remove";
-  removeButton.addEventListener("click",(e) =>removeBook(e))
+  const titleNode = createNewElement("div", "title", title.value);
+  const authorNode = createNewElement("div", "author", author.value);
+  const pagesNode = createNewElement("div", "pages", pages.value);
+  const statusNode = createNewElement("button", "status", theStatus.value);
+  const removeButton = createNewElement("button", "remove-card", "remove");
+  newBook.setAttribute("status", statusOptions.indexOf(theStatus.value));
+  statusNode.addEventListener("click", (e) => changeStatus(e));
+  removeButton.addEventListener("click", (e) => removeBook(e));
   newBook.appendChild(titleNode);
   newBook.appendChild(authorNode);
   newBook.appendChild(pagesNode);
@@ -45,12 +43,17 @@ function addNewBook(e) {
   clearInputFields();
 }
 function clearInputFields() {
-  for (let i = 0; i < submitForm.length-2;i++){
+  for (let i = 0; i < submitForm.length - 2; i++) {
     submitForm.elements[i].value = "";
   }
 }
 function removeBook(e) {
   e.target.parentNode.remove();
+}
+function changeStatus(e) {
+  let next = (parseInt(e.target.parentNode.getAttribute("status")) + 1) % 3;
+  e.target.parentNode.setAttribute("status", next);
+  e.target.textContent = statusOptions[next]
 }
 
 submitForm.addEventListener("submit", (e) => addNewBook(e));
